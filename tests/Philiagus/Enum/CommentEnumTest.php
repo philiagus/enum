971289818fdd58d54357dd2cea13b3d677afbf6b
Andreas Bittner <philiagus@philiagus.de>
2019-11-12 19:56:37 +0100

    Update copyright information

--- tests/Philiagus/Enum/CommentEnumTest.php
+++ tests/Philiagus/Enum/CommentEnumTest.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of philiagus/enum
+ *
+ * (c) Andreas Bittner <philiagus@philiagus.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace Philiagus\Test\Enum;
